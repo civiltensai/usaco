7b91b82a4fcb9972257ffe707d14d6e98002bc5d
civiltensai <civiltensai@gmail.com>
2015-03-02 17:20:49 +0800

    lamps (pass)

--- Chapter2/lamps.c
+++ Chapter2/lamps.c
@@ -7,7 +7,7 @@ TASK: lamps
 #include <stdlib.h>
 #include <string.h>
 
-#define _DEBUG_
+//#define _DEBUG_
 #ifdef _DEBUG_
 	FILE *fdebug;
 	#define debug(str, ...) do{ fprintf(fdebug, str, __VA_ARGS__); printf(str, __VA_ARGS__); }while(0)
@@ -22,85 +22,6 @@ int number;
 int counter;
 int count = 0;
 char* failString = "IMPOSSIBLE";
-
-/*
-//hash table
-int primes[MAX];
-int primeIndex = 0;
-int lastPrime = 2;
-
-int isPrime(int p){
-	if(p < 4){
-		return 1;
-	}
-	else{
-		int i;
-		for(i = 0; primes[i]*primes[i] < p; i++){
-			if(p%primes[i] == 0){
-				return 0;
-			}
-		}
-		return 1;
-	}
-}
-
-int initPrimes(int n){
-	int i = 2;
-	while(i <= n*n){
-		if(isPrime(i)){
-			lastPrime = i;
-			if(i <= n+1){
-				primes[primeIndex] = i;
-				primeIndex++;
-			}
-		}
-		i++;
-	}
-}
-*/
-
-/*
-//state in string
-char state[MAX];
-char finalState[MAX];
-void button1(void){
-	int i;
-	for(i = 0; i < number; i++){
-		state[i] = 97 - state[i];
-	}
-}
-
-void button2(void){
-	int i;
-	for(i = 0; i < number; i = i + 2){
-		state[i] = 97 - state[i];
-	}
-}
-
-void button3(void){
-	int i;
-	for(i = 1; i < number; i = i + 2){
-		state[i] = 97 - state[i];
-	}
-}
-
-void button4(void){
-	int i;
-	for(i = 1; i < number; i = i + 3){
-		state[i] = 97 - state[i];
-	}
-}
-
-int isFinalState(void){
-	int i;
-	for(i = 0; i < number; i++){
-		if(finalState[i] != 0 && state[i] != finalState[i]){
-			return 0;
-		}
-	}
-	return 1;
-}
-*/
 
 
 int stack[MAX];
@@ -120,7 +41,6 @@ typedef struct _node{
 	struct _node* on;
 	struct _node* off;
 	struct _node* parent;
-	struct _node* nextAnswer;
 	int answer;
 #ifdef _DEBUG_
 	int state[MAX];
@@ -128,8 +48,6 @@ typedef struct _node{
 #endif
 }NODE;
 NODE rootNode;
-NODE* answerPtrStart = NULL;
-NODE* answerPtrEnd = NULL;
 
 typedef struct _anwser{
 	unsigned long up;
@@ -140,6 +58,7 @@ typedef struct _anwser{
 #ifdef _DEBUG_
 	void dumpState(NODE* leaf){
 		int i;
+		debug("%s","\n");
 		for(i = 1; i <= number; i++){
 			debug("%2d", i);
 		}
@@ -172,7 +91,6 @@ NODE* creatNode(NODE* parent){
 	newNode->parent = parent;
 	newNode->off = NULL;
 	newNode->on = NULL;
-	newNode->nextAnswer = NULL;
 	newNode->answer = 0;
 	
 	return newNode;
@@ -197,7 +115,7 @@ int isFinalState(NODE* leaf){
 //Return the leaf of the next new state. If not exist this state, change isExist=0;
 void button1(NODE* leaf){
 	int i = 1;
-	
+	//debug("%s", "(1)-");
 	do{
 		if(leaf->parent->off != NULL && leaf->parent->off == leaf){	//off node
 			push(1); //button1 revert all
@@ -212,8 +130,8 @@ void button1(NODE* leaf){
 
 //Return the leaf of the next new state. If not exist this state, change isExist=0;
 void button2(NODE* leaf){
-	int i = 1;
-	
+	int i = number;
+	//debug("%s", "(2)-");
 	do{
 		if(0 ^ (i%2)){
 			if(leaf->parent->off != NULL && leaf->parent->off == leaf){	//off node
@@ -228,15 +146,15 @@ void button2(NODE* leaf){
 		}
 		
 		leaf = leaf->parent;
-		i++;
+		i--;
 	}while(leaf->parent);
 	
 }
 
 //Return the leaf of the next new state. If not exist this state, change isExist=0;
 void button3(NODE* leaf){
-	int i = 1;
-	
+	int i = number;
+	//debug("%s", "(3)-");
 	do{
 		if(1 ^ (i%2)){
 			if(leaf->parent->off != NULL && leaf->parent->off == leaf){	//off node
@@ -249,24 +167,16 @@ void button3(NODE* leaf){
 		else{
 			push(leaf->parent->on != NULL && leaf->parent->on == leaf); //on:1/off:0 mode
 		}
-		/*
-		if(leaf->parent->off != NULL && leaf->parent->off == leaf){	//off node
-			push(1 ^ (i%2==0)); //button3 revert even, i=2, 4, 6, ...
-		}
-		else{							//on node
-			push(0 ^ (i%2==0)); //button3 revert even, i=2, 4, 6, ...
-		}
-		*/
 		leaf = leaf->parent;
-		i++;
+		i--;
 	}while(leaf->parent);
 	
 }
 
 //Return the leaf of the next new state. If not exist this state, change isExist=0;
 void button4(NODE* leaf){
-	int i = 1;
-	
+	int i = number;
+	//debug("%s", "(4)-");
 	do{
 		if(0 ^ (i%3==1)){
 			if(leaf->parent->off != NULL && leaf->parent->off == leaf){	//off node
@@ -279,16 +189,8 @@ void button4(NODE* leaf){
 		else{
 			push(leaf->parent->on != NULL && leaf->parent->on == leaf); //on:1/off:0 mode
 		}
-		/*
-		if(leaf->parent->off != NULL && leaf->parent->off == leaf){	//off node
-			push(0 ^ (i%3==1)); //button4 revert 3k+1, i=1, 4, 7, ...
-		}
-		else{							//on node
-			push(1 ^ (i%3==1)); //button4 revert 3k+1, i=1, 4, 7, ...
-		}
-		*/
 		leaf = leaf->parent;
-		i++;
+		i--;
 	}while(leaf->parent);
 	
 }
@@ -333,15 +235,10 @@ void setResult(NODE* leaf){
 
 	//nextLeaf = createState(isExist);
 void findPossible(NODE* leaf, int depth){
+	debug("findPossible depth=%d", depth);
+	dumpState(leaf);
 	if(isFinalState(leaf)){
-		if(answerPtrStart == NULL){
-			answerPtrStart = leaf;
-		}
-		if(answerPtrEnd != NULL && answerPtrStart!= leaf){
-			answerPtrEnd->nextAnswer = leaf;
-		}
 		count++;
-		answerPtrEnd = leaf;
 		setResult(leaf);
 		dumpState(leaf);
 	}
@@ -349,46 +246,63 @@ void findPossible(NODE* leaf, int depth){
 		int isExist = 1;
 		button1(leaf);
 		NODE* nextLeaf = createState(&isExist);
-		setState(nextLeaf);
+		
 		if(isExist == 0){
+			debug("%s", "(1)-");
+			setState(nextLeaf);
 			findPossible(nextLeaf, depth+1);
 		}
 		
 		isExist = 1;
 		button2(leaf);
 		nextLeaf = createState(&isExist);
-		setState(nextLeaf);
+		
 		if(isExist == 0){
+			debug("%s", "(2)-");
+			setState(nextLeaf);
 			findPossible(nextLeaf, depth+1);
 		}
 		
 		isExist = 1;
 		button3(leaf);
 		nextLeaf = createState(&isExist);
-		setState(nextLeaf);
+		
 		if(isExist == 0){
+			debug("%s", "(3)-");
+			setState(nextLeaf);
 			findPossible(nextLeaf, depth+1);
 		}
 		
 		isExist = 1;
 		button4(leaf);
 		nextLeaf = createState(&isExist);
-		setState(nextLeaf);
+		
 		if(isExist == 0){
+			debug("%s", "(4)-");
+			setState(nextLeaf);
 			findPossible(nextLeaf, depth+1);
 		}
 	}
+	debug("findPossible end depth=%d", depth);
 	
 }
 
 void DFS(NODE* root){
+	static NODE* branch = NULL;
 	if(root && root->answer){
 		if(root->answer & 0x2){
 			fprintf(fout, "%d", 0);
 			debug("%d", 0);
+			if(root->answer & 0x3 == 0x3){
+				branch = root;
+			}
 			DFS(root->off);
+			if(branch != NULL){
+				branch->answer -= 0x2;
+				branch = NULL;
+			}
 		}
-		if(root->answer & 0x1){
+		else if(root->answer & 0x1){
 			fprintf(fout, "%d", 1);
 			debug("%d", 1);
 			DFS(root->on);
@@ -423,13 +337,6 @@ int main(void){
 	}
 	setState(parent);
 	
-	/*
-	//init state, set all lamps to be ON.
-	for(i = 0; i < number; i++){
-		state[i] = '1';
-	}
-	*/
-	
 	int temp=0;
 	do{
 		fscanf(fin, "%d", &temp);
@@ -459,34 +366,6 @@ int main(void){
 	ANSWER* result = (ANSWER*) malloc(sizeof(ANSWER)*count);
 	memset(result, 0, sizeof(ANSWER));
 	
-	NODE* answer = answerPtrStart;
-	j = 0;
-	while(answer){
-		NODE* leaf = answer;
-		
-		for(i = 1; i <= number; i++){
-			push(leaf->parent->on == leaf);
-			leaf = leaf->parent;
-		}
-		for(i = 0; i < number && i < 32; i++){
-			temp = pop();
-			result[j].mid = result[j].low<<1 + temp;
-			//fprintf(fout, "%d", temp);
-			//debug("%d", temp);
-		}
-		for(i = 32; i < number && i < 64; i++){
-			temp = pop();
-			result[j].mid = result[j].mid<<1 + temp;
-		}
-		for(i = 64; i < number && i < 100; i++){
-			temp = pop();
-			result[j].mid = result[j].up<<1 + temp;
-		}
-		//fprintf(fout, "%s","\n");
-		//debug("%s","\n");
-		j++;
-		answer = answer->nextAnswer;
-	}
 	
 	//print out
 	
@@ -495,7 +374,10 @@ int main(void){
 		debug("%s\n", failString);
 	}
 	else{
-		DFS(&rootNode);
+		while(count){
+			DFS(&rootNode);
+			count--;
+		}
 	}
 	
 	fclose(fin);
