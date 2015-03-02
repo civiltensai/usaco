50a2dbc496e8d6d65976a7cf5469782d1e71bd8a
阿呆 <civiltensai@gmail.com>
2015-03-03 01:41:26 +0800

    prefix (pass)

--- Chapter2/prefix.c
+++ Chapter2/prefix.c
@@ -7,47 +7,26 @@ TASK: prefix
 #include <stdlib.h>
 #include <string.h>
 
-#define _DEBUG_
+//#define _DEBUG_
 #ifdef _DEBUG_
 	FILE *fdebug;
-	#define debug(str, ...) do{ fprintf(fdebug, str, __VA_ARGS__); printf(str, __VA_ARGS__); }while(0)
+	#define debug(str, ...) do{ fprintf(fdebug, str, __VA_ARGS__); printf(str, __VA_ARGS__); fflush(fdebug);}while(0)
 #else
 	#define debug(str, ...) {;}
 #endif
 FILE *fout;
 
+#define MAXLENGTH 200001
 char primitives[201][11];
 int primitiveCount = 0;
-char sequence[200001];
+char sequence[MAXLENGTH];
+int unmatchedLen[MAXLENGTH];
 int sLength = 0;
-int maxPrefixLen = 0;
 
-void cmpLen(int len){
-	if(len > maxPrefixLen){
-		maxPrefixLen = len;
-		debug("maxPrefixLen = %d\n", maxPrefixLen);
-	}
-}
-
-
-void prefix(char* string, int length){
-	if(length==0){
-		cmpLen(sLength - length);
-	}
-	else{
-		int i, matched = 0;
-		for(i = 0; i < primitiveCount; i++){
-			int len = strlen(primitives[i]);
-			if(strncmp(string, primitives[i], len) == 0){
-				prefix(string+len, length - len);
-				matched++;
-			}
-		}
-		if(matched == 0){
-			cmpLen(sLength - length);
-		}
-	}
-}
+#define MIN(a,b) \
+   ({ __typeof__ (a) _a = (a); \
+       __typeof__ (b) _b = (b); \
+     _a < _b ? _a : _b; })
 
 int main(void){
 	FILE *fin  = fopen("prefix.in", "r");
@@ -62,20 +41,38 @@ int main(void){
 		debug("%s\n", primitives[primitiveCount]);
 		primitiveCount++;
 	}while(primitives[primitiveCount-1][0] != '.');
+	primitiveCount--;
+	debug("primitiveCount=%d\n", primitiveCount);
 	
 	char* sStart = sequence;
-	debug("aAAAAAAa%s", "\n");
+	debug("-----------------------------%s", "\n");
 	while(fscanf(fin, "%s", sStart) != EOF){
-		debug("%s\n", sStart);
+		//debug("%s\n", sStart);
 		sStart = sStart+strlen(sStart);
 	}
+	debug("-----------------------------%s", "\n");
 	sLength = strlen(sequence);
-	debug("%d %s\n", sLength, sequence);
+	debug("%d\n%s\n-----------------------------\n", sLength, sequence);
 	
-	prefix(sequence, sLength);
+	//init unmatchedLen[]
+	int i, j;
+	for(i = 0; i <= sLength; i++){
+		unmatchedLen[i] = i;
+	}
+	
+	for(j = 1; j <= sLength; j++){
+		for(i = 0; i < primitiveCount; i++){
+			int len = strlen(primitives[i]);
+			if(len <= j && strncmp(&(sequence[sLength - j]), primitives[i], len) == 0){
+				unmatchedLen[j] = MIN(unmatchedLen[j], unmatchedLen[j - len]);
+			}
+		}
+		debug("%d - %d\n", j, unmatchedLen[j]);
+	}
+	
 	//print out
-	fprintf(fout, "%d\n", maxPrefixLen);
-	debug("%d\n", maxPrefixLen);
+	fprintf(fout, "%d\n", sLength - unmatchedLen[sLength]);
+	debug("%d\n", sLength - unmatchedLen[sLength]);
 	
 	
 	fclose(fin);
