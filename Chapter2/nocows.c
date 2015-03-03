d08f588f00aaef9a84b1ff8b216e91b5a5121437
civiltensai <civiltensai@gmail.com>
2015-03-03 17:25:55 +0800

    nocows (temp)

--- Chapter2/nocows.c
+++ Chapter2/nocows.c
@@ -16,8 +16,9 @@ TASK: nocows
 #endif
 FILE *fout;
 
-int degree;
-int height;
+int nodeCount;	//3 <= N <= 200
+int height;		//1 < K < 100
+int pedigrees[200][100]; //pedigrees[n][k]: n nodes with height <= k.
 
 int main(void){
 	FILE *fin  = fopen("nocows.in", "r");
@@ -26,13 +27,32 @@ int main(void){
 	fdebug = fopen("debug.log", "w");
 #endif
 	
-	fscanf(fin, "%d %d", &degree, &height);
-	debug("%d %d\n", degree, height);
+	fscanf(fin, "%d %d", &nodeCount, &height);
+	debug("%d %d\n", nodeCount, height);
 	
+	int i, j;
+	for(i = 0; i < nodeCount; i++){
+		for(j = 0; j < nodeCount; j++){
+			pedigrees[i][j] = 0;
+		}
+	}
+	pedigrees[1][1] = 1;
+	pedigrees[3][2] = 1;
+	
+	int n, k;
+	for(n = 5; n <= nodeCount; n=n+2){							// number of nodes
+		for(k = 2; k <= height ; k++){							// height
+			for(i = 0 ; i <= n-1; i++){		// numbder of nodes in left subtree
+				pedigrees[n][k] += pedigrees[i][k-1]*pedigrees[n-1-i][k-1];
+				debug("pedigrees[%d][%d]=%d pedigrees[%d][%d]=%d pedigrees[%d][%d]=%d\n", n, k, pedigrees[n][k], i, k-1, pedigrees[i][k-1], n-1-i, k-1, pedigrees[n-1-i][k-1]);
+			}
+			debug("n=%d k=%d => %d\n", n, k, pedigrees[n][k]);
+		}
+	}
 	
 	//print out
-	//fprintf(fout, "%d\n", sLength - unmatchedLen[sLength]);
-	//debug("%d\n", sLength - unmatchedLen[sLength]);
+	fprintf(fout, "%d\n", pedigrees[nodeCount][height] - pedigrees[nodeCount][height-1]);
+	debug("%d\n", pedigrees[nodeCount][height] - pedigrees[nodeCount][height-1]);
 	
 	
 	fclose(fin);
